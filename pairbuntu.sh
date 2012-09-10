2cbdf126cfa074d9bcaf4a63d0fbc060fe5590a6
Dave Guarino <daguar@gmail.com>
2012-09-10 14:35:50 -0400

    Fixing by removing su

--- pairbuntu.sh
+++ pairbuntu.sh
@@ -32,11 +32,8 @@ sed -i 's/PasswordAuthentication\ no/PasswordAuthentication\ yes/' /etc/ssh/sshd
 echo -e "\nRestarting ssh to enable password authentication.\n\n"
 service ssh restart
 
-echo -e "\nSwitching to coder1; enter password.\n\n"
-su coder1
-cd ~
-touch ~/.screenrc
-echo -e "multiuser on\nacladd coder2" > ~/.screenrc
+echo -e "multiuser on\nacladd coder2" > /home/coder1/.screenrc
+chown /home/coder1/.screenrc coder1
 
 echo -e "\nw00t. Setup is complete.\n\nNext, have your buddy log in via ssh as coder2, using the password you set up for that account.\n\n  ssh coder2@ec2-9999-9999-9999-9999.compute-1.amazon.com\n\n  (Remember to replace the ec2-999... above with your own instance's address.)\n\Then, you as coder1 type:\n\n  screen -S pairprog\n\nto start your pairing session, and have your buddy run\n\n  screen -x coder1/pairprog\n\nafter logging in to enter your shared screen.\n\nFor more help, use 'man screen' to learn more about how screen works."
 
